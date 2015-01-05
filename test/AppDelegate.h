e7b5c2858c0eb760038427ade416bc576f2585cd
张永盛 <mac@zhangyongsheng.local>
2015-01-05 16:22:49 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 张永盛 on 15/1/5.
+//  Copyright (c) 2015年 zhangyongsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 张永盛 on 15/1/5.
+//  Copyright (c) 2015年 zhangyongsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
